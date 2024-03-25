b3ed5361e7a0d8acedb50196afe931c01b90aa5c
Wagner Terry <goncalveswagner15@gmail.com>
2024-03-24 21:31:28 -0300

    feat: adicionando os endpoints restantes de parceiros

--- app-list-partners/src/services/APIService.ts
+++ app-list-partners/src/services/APIService.ts
@@ -2,15 +2,36 @@ import axios from "axios";
 
 const BaseURL = "https://644060ba792fe886a88de1b9.mockapi.io/v1/test/partners";
 
-// Listar tudo - GET /
-// Listar um por id - GET /:id
-// Cadastrar um parceiro - POST - /
-// Atualizar um parceiro - PUT - /:id
-// Deletar um parceiro - DELETE - /:id
+interface PartnerFormEditProps {
+  id?: string;
+  name?: string;
+  description?: string;
+}
 
 export default class APIService {
   static getPartners = async () => {
     const result = await axios.get(`${BaseURL}`);
+    return result.data;
+  };
+
+  static getPartnersByCode = async (id: string) => {
+    const result = await axios.get(`${BaseURL}/${id}`);
+    return result.data;
+  };
+
+  static savePartner = async (data: string) => {
+    const result = await axios.post(`${BaseURL}`, data);
+    return result.data;
+  };
+
+  static updateProduct = async (id: string, data: PartnerFormEditProps) => {
+    const { name, description } = data;
+    const payload = {
+      id,
+      name,
+      description,
+    };
+    const result = await axios.put(`${BaseURL}/${id}`, payload);
     return result.data;
   };
 
